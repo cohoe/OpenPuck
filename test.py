e30d1627583a68ee27c8e8fa6b4cb0338cefc247
Grant Cohoe <grant.cohoe@rsa.com>
2015-07-12 17:09:57 -0400

    removed old test script

--- test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from hawkeyapi import ScheduleParser
-
-url = 'http://ritathletics.com/schedule.aspx?path=whock'
-
-json_schedule = ScheduleParser.parse2json(url)
-for game in json_schedule:
-    print game
