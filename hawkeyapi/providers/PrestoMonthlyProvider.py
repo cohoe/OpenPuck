29a9cb1f59d018426f4033d5d6972ad5801b789d
Grant Cohoe <grant.cohoe@rsa.com>
2015-09-11 22:03:44 -0400

    Made prestomonthly ignore useless rows

--- hawkeyapi/providers/PrestoMonthlyProvider.py
+++ hawkeyapi/providers/PrestoMonthlyProvider.py
@@ -45,7 +45,12 @@ class PrestoMonthlyProvider(Provider):
                 if re.search(r'[a-zA-Z]{3}', game.text):
                     month = game.text
                 continue
+            # @TODO: Why is this here?
             if game['class'][0] == "month-sep":
+                continue
+
+            # See if there is actually a game here.
+            if not game.find('td', class_='e_date'):
                 continue
 
             # They do not provide a common location field, so
--- scripts/tests/test_providers.py
+++ scripts/tests/test_providers.py
@@ -8,34 +8,41 @@ from hawkeyapi.TestData import seasons
 s = seasons[1]
 
 #teams = TeamModel.scan(is_women__eq=True)
-teams = TeamModel.conference_index.query('WCHA', is_women__eq=False)
-#teams = TeamModel.conference_index.query('WCHA', is_women__eq=False, name__begins_with='University of Alaska')
+teams = TeamModel.conference_index.query('NCHC', is_women__eq=False)
+#teams = TeamModel.conference_index.query('WCHA', is_women__eq=False, name__begins_with='Northern')
 
 for e in teams:
-    t = Team(
-        e.name,
-        e.mascot,
-        e.is_women,
-        e.home_conference,
-        e.social_media,
-        e.web_site,
-        e.web_provider,
-    )
+    try:
+        t = Team(
+            e.name,
+            e.mascot,
+            e.is_women,
+            e.home_conference,
+            e.social_media,
+            e.web_site,
+            e.web_provider,
+        )
+
+        team_provider = t.get_provider()
+    except Exception as ex:
+        print "Totally blew up on %s" % e.name
+        print "  Specific error: %s" % ex
+        continue
 
     print ""
     print "%s - %s" % (t.institution_name, s.id)
 
-    team_provider = t.get_provider()
 
-    sched_entries = team_provider.get_schedule(s)
-    print "%s Parsed %i entries" % (t.provider, len(sched_entries))
+#    sched_entries = team_provider.get_schedule(s)
+#    print "%s Parsed %i entries" % (t.provider, len(sched_entries))
 
-#    try:
-#        sched_entries = team_provider.get_schedule(s)
-#        print "%s Parsed %i entries" % (t.provider, len(sched_entries))
-#    except Exception as e:
-#        print "FAILED"
-#        print "  Specific error: %s" % e
-#        continue
-#
-#    print "SUCCESS"
+
+    try:
+        sched_entries = team_provider.get_schedule(s)
+        print "%s Parsed %i entries" % (t.provider, len(sched_entries))
+    except Exception as e:
+        print "FAILED"
+        print "  Specific error: %s" % e
+        continue
+
+    print "SUCCESS"
